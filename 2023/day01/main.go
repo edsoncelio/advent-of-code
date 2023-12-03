8c001bac090dc0e59e6726f627a7013996903a07
edsoncelio <edsoncelio2020@gmail.com>
2023-12-03 18:45:18 -0300

    feat: add day01

--- 2023/day01/main.go
+++ 2023/day01/main.go
@@ -6,58 +6,47 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-func numberLiteral(s string) string {
-	switch number := s; number {
-	case "one":
-		return "1"
-	case "two":
-		return "2"
-	case "three":
-		return "3"
-	case "four":
-		return "4"
-	case "five":
-		return "5"
-	case "six":
-		return "6"
-	case "seven":
-		return "7"
-	case "eight":
-		return "8"
-	case "nine":
-		return "9"
-	default:
-		return ""
-	}
-}
-
 // part 02
-func wordToNumber(word string, position string) string {
-	var digitString string
-
-	pattern := regexp.MustCompile("(one|two|three|four|five|six|seven|eight|nine)")
-	matches := pattern.FindAllString(word, -1)
-
-	if len(matches) > 0 {
-		if position == "start" {
-			digitString = numberLiteral(matches[0])
-		}
-		if position == "end" {
-			digitString = numberLiteral(matches[len(matches)-1])
-		}
-		if position == "all" {
-			for _, matche := range matches {
-				digitString = digitString + numberLiteral(matche)
+func wordToNumber(word string) string {
+	resultWord := word
+	var tmpResult string
+	listNumbers := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	for _, digit := range listNumbers {
+		tmpResult = resultWord
+		if strings.Contains(word, digit) {
+			if digit == "one" {
+				resultWord = strings.Replace(tmpResult, digit, fmt.Sprintf("%s%s%s", string(digit[0]), "1", string(digit[len(digit)-1:])), -1)
+			}
+			if digit == "two" {
+				resultWord = strings.Replace(tmpResult, digit, fmt.Sprintf("%s%s%s", string(digit[0]), "2", string(digit[len(digit)-1:])), -1)
+			}
+			if digit == "three" {
+				resultWord = strings.Replace(tmpResult, digit, fmt.Sprintf("%s%s%s", string(digit[0]), "3", string(digit[len(digit)-1:])), -1)
+			}
+			if digit == "four" {
+				resultWord = strings.Replace(tmpResult, digit, fmt.Sprintf("%s%s%s", string(digit[0]), "4", string(digit[len(digit)-1:])), -1)
+			}
+			if digit == "five" {
+				resultWord = strings.Replace(tmpResult, digit, fmt.Sprintf("%s%s%s", string(digit[0]), "5", string(digit[len(digit)-1:])), -1)
+			}
+			if digit == "six" {
+				resultWord = strings.Replace(tmpResult, digit, fmt.Sprintf("%s%s%s", string(digit[0]), "6", string(digit[len(digit)-1:])), -1)
+			}
+			if digit == "seven" {
+				resultWord = strings.Replace(tmpResult, digit, fmt.Sprintf("%s%s%s", string(digit[0]), "7", string(digit[len(digit)-1:])), -1)
+			}
+			if digit == "eight" {
+				resultWord = strings.Replace(tmpResult, digit, fmt.Sprintf("%s%s%s", string(digit[0]), "8", string(digit[len(digit)-1:])), -1)
+			}
+			if digit == "nine" {
+				resultWord = strings.Replace(tmpResult, digit, fmt.Sprintf("%s%s%s", string(digit[0]), "9", string(digit[len(digit)-1:])), -1)
 			}
 		}
-	} else {
-		fmt.Println(matches)
 	}
-
-	return digitString
-
+	return resultWord
 }
 
 func isNumber(s string) bool {
@@ -99,46 +88,16 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 
 	for fileScanner.Scan() {
-		if isNumber(fileScanner.Text()[len(fileScanner.Text())-1:]) && isNumber(string(fileScanner.Text()[0])) {
-			result := string(fileScanner.Text()[0]) + fileScanner.Text()[len(fileScanner.Text())-1:]
-			if len(result) == 1 {
-				result = result + result
-			}
-			fmt.Printf("Original string (all number): %s - Numbers: %s\n", fileScanner.Text(), result)
-			sum, _ := strconv.Atoi(result)
-			sumAll += sum
-			continue
+		textoToNumber := wordToNumber(fileScanner.Text())
+		extractNumber := isNumeric(textoToNumber)
+
+		if len(extractNumber) == 1 {
+			extractNumber = extractNumber + extractNumber
 		}
 
-		if isNumber(fileScanner.Text()[len(fileScanner.Text())-1:]) {
-			result := wordToNumber(fileScanner.Text(), "start")
-			if len(result) == 1 {
-				result = result + result
-			}
-			if len(result) > 2 {
-				result = string(result[0]) + result[len(result)-1:]
-			}
-			fmt.Printf("Original string (second number): %s - Numbers: %s\n", fileScanner.Text(), string(result[0])+fileScanner.Text()[len(fileScanner.Text())-1:])
-			sum, _ := strconv.Atoi(result)
-			sumAll += sum
-
-			continue
-		}
-
-		if !isNumber(fileScanner.Text()[len(fileScanner.Text())-1:]) && !isNumber(string(fileScanner.Text()[0])) {
-			result := wordToNumber(fileScanner.Text(), "all")
-			fmt.Printf("Original string (all text): %s - Numbers: %s\n", fileScanner.Text(), result)
-			continue
-		}
-
-		// resultWord := wordToNumber(fileScanner.Text())
-		// fmt.Printf("Original string: %s - Numbers: %s\n", fileScanner.Text(), resultWord)
-
-		// number, err := strconv.Atoi(resultLine)
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// sumAll = sumAll + number
+		fmt.Printf("Original string: %s - Numbers: %s\n", fileScanner.Text(), extractNumber)
+		sum, _ := strconv.Atoi(extractNumber)
+		sumAll += sum
 	}
 
 	fmt.Printf("Total sum: %d\n", sumAll)
